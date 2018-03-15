8fe22f610a1f0a37a5372d3d9fb6dba7b0451982
Chia T <tham_oh@hotmail.com>
2018-03-15 01:01:35 -0500

    add JS hoisting

---
+++ 01-JS_Fundamentals/08-Hoisting.js
@@ -0,0 +1,98 @@
+// 1
+// console.log(hello);
+// var hello = 'world';
+
+//**** Answer below ****/
+// world (should be undefined)
+
+
+// 2
+// var needle = 'haystack';
+// test();
+// function test(){
+// 	var needle = 'magnet';
+// 	console.log(needle);
+// }
+
+//**** Answer below ****/
+// magnet
+
+// 3
+// var brendan = 'super cool';
+// function print(){
+// 	brendan = 'only okay';
+// 	console.log(brendan);
+// }
+
+//**** Answer below ****/
+// console.log(brendan);
+// super cool
+
+// 4
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+// 	food = 'half-chicken';
+// 	console.log(food);
+// 	var food = 'gone';
+// }
+
+//**** Answer below ****/
+// chicken
+// half-chicken
+
+// 5
+// mean();
+// console.log(food);
+// var mean = function() {
+// 	food = "chicken";
+// 	console.log(food);
+// 	var food = "fish";
+// 	console.log(food);
+// }
+// console.log(food);
+
+//**** Answer below ****/
+// chicken
+// fish
+// undefine
+// undefine
+// (answer is TypeError: mean is not a function)
+
+// 6
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+// 	genre = "rock";
+// 	console.log(genre);
+// 	var genre = "r&b";
+// 	console.log(genre);
+// }
+// console.log(genre);
+
+//**** Answer below ****/
+// undefined
+// rock
+// r&b
+// undefined (should be disco)
+
+
+// 7
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+// 	dojo = "seattle";
+// 	console.log(dojo);
+// 	var dojo = "burbank";
+// 	console.log(dojo);
+// }
+// console.log(dojo);
+
+//**** Answer below ****/
+//san jose
+//seattle
+//burbank
+//san jose

9fc97447ff261caff72807db8d2f43ff5ef5626b
Chia T <tham_oh@hotmail.com>
2018-03-27 22:29:00 -0500

    bug fiexed -  JS Adv, DojoQuery

--- 05-DojoQuery/DojoQuery.js
+++ 05-DojoQuery/DojoQuery.js
@@ -1,19 +1,27 @@
-function $Dojo(element) {
-    var id = document.getElementById(element);
+$(document).ready(function() {
 
-    if (id) {
-        this.click = function (callback) {
-            this.id.addEventListener('click', callback);
+    function $Dojo(element) {
+        var id = document.getElementById(element);
+
+        if (id) {
+            this.click = function (callback) {
+                id.addEventListener('click', callback);
+            }
+        
+            this.hover = function (callback1, callback2) {
+                id.addEventListener('mouseover', callback1);
+                id.addEventListener('mouseout', callback2);
+            }
+            return this;
         }
-    
-        this.hover = function (callback1, callback2) {
-            this.id.addEventListener('mouseover', callback1);
-            this.id.addEventListener('mouseout', callback2);
-        }
-        return this;
-    }
-};
+    };
+
+    $Dojo("buttonClick").click(function() { console.log("The button was clicked!") });
+    $Dojo("buttonHover").hover(
+        function() {console.log("The button was hovered ON!")},
+        function() {console.log("The button was hovered OUT!")},
+    );
+
+});
 
 
-$Dojo("buttonClick").click(function() { console.log("The button was clicked!") });
-$Dojo("buttonHover").hover(function() { console.log("The button was hovered on!") });
